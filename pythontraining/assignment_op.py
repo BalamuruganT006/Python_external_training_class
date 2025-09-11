3798c113c0b35c2a2b05ecc0b5c8b017e66f029b
Balamurugan T <bala4256t@gmail.com>
2025-09-11 16:29:19 +0530

    Create assignment_op.py

---
+++ pythontraining/assignment_op.py
@@ -0,0 +1,22 @@
+x=10
+
+x+=10   #x=x+10
+print(x)
+
+x-=10      #x=x-10
+print(x)
+
+x*=10     #x=x*10
+print(x)
+
+x/=10    #x=x/10
+print(x)
+
+"""x%=10
+print(x)
+
+x//=10
+print(x)"""
+
+x**=2
+print(x)
