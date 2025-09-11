b89266b4a604559d5d507ac91ecdf290a583c72b
Balamurugan T <bala4256t@gmail.com>
2025-09-11 15:51:54 +0530

    arithmeticoperator

---
+++ pythontraining/arithmetic_operaters.py
@@ -0,0 +1,39 @@
+#operator task 
+
+#task 1
+a=int(input("enter the num: "))
+b=int(input("enter the num: "))
+print(a,b)
+#add,sub,
+c=a+b
+
+d=a-b
+print(c)
+print(d)
+
+#multi
+e=a*b
+print(e)
+
+#div
+f=b/a
+print(f)
+
+#mod
+e=a%b
+print(e)
+
+f=b%a
+print(f)
+g=a%100
+print(g)
+h=b%10
+print(h)
+i=a//b
+print(i)
+j=b//a
+print(j)
+k=a**b
+print(k)
+l=b**a
+print(l)
