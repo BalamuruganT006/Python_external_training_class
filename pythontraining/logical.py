dca1aec448172e933335299baed0978900108337
Balamurugan T <bala4256t@gmail.com>
2025-09-11 16:29:07 +0530

    Create logical.py

---
+++ pythontraining/logical.py
@@ -0,0 +1,21 @@
+# logical and operator
+a=True
+b=False
+
+# basic and , or ,not operator
+
+print( a and b)
+if(a and b):
+    print("true")
+else:
+    print("false")    
+
+#print(a or b)
+
+print(not a and not b)
+
+
+#special behaviour     
+
+print(5 and 10)           
+print(5 or 10)
