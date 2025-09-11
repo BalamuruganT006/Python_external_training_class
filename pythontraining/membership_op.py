3162692892c9e6723c2a0afcbb87c9bedb6bd84a
Balamurugan T <bala4256t@gmail.com>
2025-09-11 17:27:06 +0530

    Create membership_op.py

---
+++ pythontraining/membership_op.py
@@ -0,0 +1,32 @@
+#membership operation
+
+#declare the str
+s='cat'
+
+#check if a is in the string or not 
+if 'a' in s:
+    print('Yes')
+else:
+    print('No')
+
+
+#check if a is not in the string or not 
+
+if 'A' not in s:
+    print('Yes')
+else:
+    print('No')
+
+print(s) 
+
+
+#in operation in list
+lst=[1,2,3,4,5,6,7,8]
+x=10
+if x in lst:
+    print('IN')
+else:
+    print("NOT IN")
+
+
+
