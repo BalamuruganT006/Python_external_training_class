e7f041a6b78818143190ec1d9b8bc192f17229f3
Balamurugan T <bala4256t@gmail.com>
2025-09-11 16:28:56 +0530

    Update relational_opp.py

--- pythontraining/relational_opp.py
+++ pythontraining/relational_opp.py
@@ -1,18 +1,18 @@
 #relatinal operator
-"""
+
 x=int(input("enter the num :"))
 y=int(input("entyer the num :"))
 
-#print(x==y)#check values
+print(x==y)#check values
 
-#print(x!=y)
+print(x!=y)
 
-#print(x>y)
+print(x>y)
 
 
-#print(x < y)"""
+print(x < y)
 
-"""#intervew
+#intervew
 
 year=int(input("enter the year:"))
 if (year % 400 == 0) or ((year % 4 == 0) & (year % 100 != 0)):
@@ -20,12 +20,14 @@ if (year % 400 == 0) or ((year % 4 == 0) & (year % 100 != 0)):
 else:
     print("not a leap year")
 
-"""
+
 #grater than or equal to , less than or equal to
-"""mark=int(input("enter the num: "))
+
+
+mark=int(input("enter the num: "))
 if(mark>=90 & mark<=100):
  print ("Distinction")
 elif(mark>=50):
     print("Pass")
 else:
-    print("Fail")"""
+    print("Fail")
