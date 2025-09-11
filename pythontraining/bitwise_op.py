d09e7e3fdbf8b2b72efbdb8f96994f6c29fc3aaf
Balamurugan T <bala4256t@gmail.com>
2025-09-11 21:32:45 +0530

    Create bitwise_op.py

---
+++ pythontraining/bitwise_op.py
@@ -0,0 +1,25 @@
+a=10
+b=4
+
+#bitwise and
+print(a & b)
+
+#bitwise or
+print(a | b)
+
+#bitwise ex-or
+print(a ^ b)
+
+#bitwise not
+print( ~ a)
+
+#right shift
+
+a=8
+print(a>>1)
+
+#left shift
+print(a<<1)
+
+a=10
+print(a << 4)
