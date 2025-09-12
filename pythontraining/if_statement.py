4f1af30ff7961ffae6ebc3d91588a16da10efe20
Balamurugan T <bala4256t@gmail.com>
2025-09-12 14:59:15 +0530

    Update if_statement.py

--- pythontraining/if_statement.py
+++ pythontraining/if_statement.py
@@ -1,34 +1,38 @@
-"""x=20
+x=20
 
 if x < 100 :
-    print("X is less than 100")"""
+    print("X is less than 100")
     
-"""x=50
+x=50
 
+
+#task 1
 if x < 100 :
-    print("number is less than 100")"""
+    print("number is less than 100")
 
-""""x=75
+x=75
 y=120
 
 if x > 50 and y < 150 :
     print("X is greater  than 50 and y is less than 150")
-"""
 
-"""x=15
+
+#task 2
+x=15
 y=40
 z=100
 
 if x * x < y and z != z :
-    print("X times y is less than z and y is not equal")"""
+    print("X times y is less than z and y is not equal")
 
 
-"""m=300
+#task 3
+m=300
 n=20
 k=15
 
 if((m//n)>k or (m%n)==0):
- print("m divied by n is greater than k or m is divisible by n")"""
+ print("m divied by n is greater than k or m is divisible by n")
 
 
 
