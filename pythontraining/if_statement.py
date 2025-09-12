39bcab2bda59d442702a27aaec26578d446a4941
Balamurugan T <bala4256t@gmail.com>
2025-09-12 14:53:06 +0530

    Create if_statement.py

---
+++ pythontraining/if_statement.py
@@ -0,0 +1,34 @@
+"""x=20
+
+if x < 100 :
+    print("X is less than 100")"""
+    
+"""x=50
+
+if x < 100 :
+    print("number is less than 100")"""
+
+""""x=75
+y=120
+
+if x > 50 and y < 150 :
+    print("X is greater  than 50 and y is less than 150")
+"""
+
+"""x=15
+y=40
+z=100
+
+if x * x < y and z != z :
+    print("X times y is less than z and y is not equal")"""
+
+
+"""m=300
+n=20
+k=15
+
+if((m//n)>k or (m%n)==0):
+ print("m divied by n is greater than k or m is divisible by n")"""
+
+
+
