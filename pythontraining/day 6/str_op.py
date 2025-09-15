4dd381853f37770f8f9849bbdd0b4fec0a77cda5
Balamurugan T <bala4256t@gmail.com>
2025-09-15 17:23:58 +0530

    Update str_op.py

--- pythontraining/day 6/str_op.py
+++ pythontraining/day 6/str_op.py
@@ -41,7 +41,7 @@ for i in p:
     print(i)
 """
 
-p1='Hello'
+"""p1='Hello'
 p2='world'
 print(p1[3])
 print(p2[3])
@@ -52,3 +52,59 @@ print(id(p2))
 print(id(p1[3]))
 print(id(p2[3]))
 print(id(p1[2]))
+"""
+
+"""
+s1 = "balamurugan"
+s2 = "balamurugan"
+
+print(s1, s2) 
+
+print(id(s1), id(s2))# address of str 
+
+print(s1[2], s1[3], s2[3])
+
+print(id(s1[2]), id(s1[3]), id(s2[3]))
+
+
+print(s1[4], s2[1])# all the o's
+
+print(id(s1[4]), id(s2[1]))"""
+
+
+
+# STRING SCLICING 
+"""
+s="python"
+i=0
+print( s[i : len(s) : 1])
+
+print(s[5:3:-1])
+"""
+
+s=input("enter the name: ")
+
+#type 1
+"""
+print(s[0:3:1])
+print(s[1:4:1])
+print(s[2:5:1])"""
+
+#type 2  using(loop)
+
+"""for i in range(0, len(s) - 2):
+    k = ''
+    for j in range(i, i + 3):
+        k += s[j]
+    print(k)"""
+
+
+#type 3 (using indexing)
+for i in range(len(s)-2):
+
+     k = s[i:i+3]
+       
+     print(k)    
+
+
+
