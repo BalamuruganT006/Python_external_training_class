5dee66ba0642dd19a5236acf685d97caab54fd7a
Balamurugan T <bala4256t@gmail.com>
2025-09-15 14:40:14 +0530

    Create str_op.py

---
+++ pythontraining/day 6/str_op.py
@@ -0,0 +1,34 @@
+'''
+ s=''
+ print(s)
+'''
+
+#type 2
+"""p='s'
+print(p)
+"""
+
+#type 3
+"""p="python"
+print(p)"""
+
+#type 4
+"""s='''
+      python
+      java 
+      c
+    '''
+print(s) """
+
+"""#type 5
+s=str(99.9)
+print(s)
+print(type(s))"""
+
+"""#escape sequence/character 
+
+print( 'Pratice makes \'prefect\' '  )
+
+
+
+print("\"Pratice\" makes 'perfect")"""
