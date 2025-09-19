16ecc05ba808f6aecc8859f3dc4e217749023dd1
Balamurugan T <bala4256t@gmail.com>
2025-09-19 17:24:33 +0530

    Create aruments.py

---
+++ pythontraining/day 10/aruments.py
@@ -0,0 +1,22 @@
+
+
+#[postional arumenr]
+
+def power(a,b):
+    c=a**b
+    print(c)
+
+power(2,5) # op[32]
+power(5,2) #op[25]
+
+
+
+def mul (a,b,c):
+    d = a+b*c
+    print(d) 
+
+mul(1,2,3)
+mul(3,2,1)
+mul(3,1,2)
+
+
