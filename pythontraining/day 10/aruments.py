f6e7419a2dd62b56d099826878fca9f2b0a2102a
Balamurugan T <bala4256t@gmail.com>
2025-09-20 10:46:17 +0530

    Update aruments.py

--- pythontraining/day 10/aruments.py
+++ pythontraining/day 10/aruments.py
@@ -19,4 +19,39 @@ mul(1,2,3)
 mul(3,2,1)
 mul(3,1,2)
 
+# default arugment
 
+#parameter without a default follows parameter with a default
+
+def power(a,b,c=2):
+    d=a*b*c
+    print(d)
+
+power(2,3,4) 
+
+
+#  [keyword arugment]
+
+def power(a,b,c):
+    d=a*b*c
+    print(d)
+
+power(a=2,b=3,c=4) 
+
+#variable length arg
+ 
+def pizza_toppings(*toppings):
+    print(toppings)
+
+
+pizza_toppings("cheese","onions","olives")
+
+
+
+#variable keyword arg
+
+def student(**data):
+    print(data)
+
+
+student(name="bala",age=21,add="tiruchengode")
